171c5798fc1f39aa6f455615673cc0a4fcd8bc4e
Krishna Choudhary <krishu1501@gmail.com>
2021-04-09 03:28:11 +0530

    added login and register forms

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'a33513838c99d7653d18890b97824e81'
 
 posts =  [
     {
@@ -25,5 +28,24 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Sign Up", form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'abc@123.com' and form.password.data == 'password':
+            flash('You have been Logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful','danger')
+    return render_template('login.html', title="Login", form=form)
+    
 if __name__ == '__main__':
     app.run(debug=True)
