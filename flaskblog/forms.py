fb6d660d663db29ee12741556e6cb42217004a7f
Krishna Choudhary <krishu1501@gmail.com>
2021-04-12 23:11:27 +0530

    added form to update account details

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
@@ -25,3 +27,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(), Length(min=2,max=20)])
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','jpeg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data!=current_user.username:
+            user =  User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is taken.Please choose another one.')
+    
+    def validate_email(self,email):
+        if email.data!=current_user.email:
+            user =  User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email is taken.Please choose another one.')
